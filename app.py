0ff6609a7c921d4cbf63dcc5afef6727a50543a9
ballantines1982 <68142239+ballantines1982@users.noreply.github.com>
2022-11-16 21:32:59 +0100

    Added basic stuff

--- app.py
+++ app.py
@@ -1,9 +1,25 @@
 from flask import Flask
+import os
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 
-@app.route
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
+
+class Meal(db.Models):
+    id = db.Column(db.Interger, primary_key=True)
+    meal = db.Column(db.String(100))
+
+
+
+
+@app.route('/')
+def hello():
+    return "Flask App"
 
 
 if __name__ == "__main__":
-    pass
+    app.run()
